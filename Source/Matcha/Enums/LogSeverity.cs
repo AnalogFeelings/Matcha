f516c54754882ba254dc186b35c71e37ffa2ce96
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-03-29 01:55:28 +0100

    Add basic project files.

---
+++ Source/Matcha/Enums/LogSeverity.cs
@@ -0,0 +1,69 @@
+﻿#region License Information (MIT)
+// Matcha - A simple but neat logging library for .NET.
+// Copyright (C) 2022-2024 Analog Feelings
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+namespace Matcha.Enums;
+
+/// <summary>
+/// Denotes different log message severities.
+/// </summary>
+[Flags]
+public enum LogSeverity
+{
+    /// <summary>
+    /// Used to log debug data for developers.
+    /// </summary>
+    /// <remarks>
+    /// This severity should be filtered on release builds.
+    /// </remarks>
+    Debug = 1,
+    
+    /// <summary>
+    /// Used to log non-urgent information about the program's execution.
+    /// </summary>
+    Information = 2,
+    
+    /// <summary>
+    /// Used to log a successful operation's completion.
+    /// </summary>
+    Success = 4,
+    
+    /// <summary>
+    /// Used to log a potentially dangerous situation.
+    /// </summary>
+    Warning = 8,
+    
+    /// <summary>
+    /// Used to log an error in the program's execution.
+    /// </summary>
+    Error = 16,
+    
+    /// <summary>
+    /// Used to log a catastrophic error which cannot be recovered from.
+    /// </summary>
+    Fatal = 32,
+    
+    /// <summary>
+    /// Convenient alias for all of the log severities.
+    /// </summary>
+    All = Debug | Information | Success | Warning | Error | Fatal
+}
